e78e5478625f510b69fb0c843bc50fdcb93baa55
KussymYerke <e_kusym@kbtu.kz>
2024-03-08 16:38:31 +0500

    check

---
+++ src/components/Header/NavItem.js
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
+
+const NavItem = ({ navItem = {} }) => {
+  const { pathname } = useRouter();
+
+  const { name, href, subNavItems } = navItem;
+  const subHref = subNavItems.map((item) => item.href);
+  const current = pathname === href || subHref.includes(pathname);
+
+  return (
+    <li className={`dropdown${current ? " current" : ""}`}>
+      <Link href={href}>
+        <div>{name}</div>
+      </Link>
+      <ul>
+        {subNavItems.map((subItem) => (
+          <li
+            className={subItem.subItems?.length ? "dropdown" : ""}
+            key={subItem.id}
+          >
+            <Link href={subItem.href}>
+              <div>{subItem.name}</div>
+            </Link>
+            <ul>
+              {subItem.subItems?.map((item) => (
+                <li key={item.id}>
+                  <Link href={item.href}>
+                    <div>{item.name}</div>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </li>
+  );
+};
+
+export default NavItem;
---
+++ src/data/mainSliderData.js
@@ -0,0 +1,13 @@
+import bg1 from "@/images/backgrounds/main-slider-1-1.png";
+
+
+const mainSliderData = [
+  {
+    id: 1,
+    bg: bg1,
+    subTitle: "Inspire yourself with the beauty of Kazakhstan with us",
+    title: "Almaty Inspiration Tour",
+  },
+];
+
+export default mainSliderData;
